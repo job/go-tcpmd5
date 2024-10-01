d5c3877f73a2ba5662f91aab6781b40fb8caabb6
Job Snijders <job@sobornost.net>
2024-10-01 10:29:16 +0000

    Remove build tag from exported func

--- tcpmd5.go
+++ tcpmd5.go
@@ -12,8 +12,6 @@
 // implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//go:build !linux && !openbsd && !windows
-// +build !linux,!openbsd,!windows
 
 package tcpmd5
 
