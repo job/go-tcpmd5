2599f5a4394835b9c4c30e3b69245d137ed83b49
Job Snijders <job@sobornost.net>
2024-10-01 10:21:53 +0000

    Remove build tags as per recommendation from @benjojo

--- tcpmd5_bsd.go
+++ tcpmd5_bsd.go
@@ -12,8 +12,6 @@
 // implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//go:build dragonfly || freebsd || netbsd
-// +build dragonfly freebsd netbsd
 
 package tcpmd5
 
